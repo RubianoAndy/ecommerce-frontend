0d228d8b222c1141970170f95cbd4420635713d6
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-10-19 17:03:01 -0500

    feat adding menu to navbar

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    NgClass,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
--- src/app/web/components/home/home.component.ts
+++ src/app/web/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CarouselComponent } from '../../../shared/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    HeaderComponent,
     CarouselComponent,
   ],
   templateUrl: './home.component.html',
