08323e1635058f36332d69f159e57a0176ee7b4a
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-10-22 16:17:50 -0500

    feat: adding data to environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
+    production: false,
+    
     dark_logo: 'assets/images/logos/Logo carnivorous plants division black.png',
     light_logo: 'assets/images/logos/Logo carnivorous plants division white.png',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
+    production: true,
+    
     dark_logo: 'assets/images/logos/Logo carnivorous plants division black.png',
     light_logo: 'assets/images/logos/Logo carnivorous plants division white.png',
 };
