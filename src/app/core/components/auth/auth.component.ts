c5ea478e567fd3d128aa718458af122442a21a28
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-10-22 21:09:57 -0500

    feat: implement loading view

--- src/app/core/components/auth/auth.component.ts
+++ src/app/core/components/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { environment } from '../../../../environments/environment.development';
+import { LoadingService } from '../../../shared/services/loading/loading.service';
 
 @Component({
   selector: 'app-auth',
@@ -12,6 +13,20 @@ import { environment } from '../../../../environments/environment.development';
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   logo = environment.light_logo;
+
+  initLoading: number = 1;
+
+  constructor (
+    private loadingService: LoadingService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loadingService.show();
+
+    setTimeout(() => {
+      this.loadingService.hide();
+    }, 1000 * this.initLoading);
+  }
 }
---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,28 @@
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { LoadingService } from '../../services/loading/loading.service';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [
+    NgIf,
+  ],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent implements OnInit {
+  logo = environment.light_logo;
+  isLoading = false;
+
+  constructor (
+    private loadingService: LoadingService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loadingService.loading$.subscribe((loading: boolean) => {
+      this.isLoading = loading;
+    });
+  }
+}
