f1db2f857e88e8f36f057ce7e04ffc59a9750aae
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-10-31 12:18:07 -0500

    fix: modify interceptor ignore register request

--- src/app/core/interceptors/auth/auth.interceptor.ts
+++ src/app/core/interceptors/auth/auth.interceptor.ts
@@ -10,7 +10,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     '/refresh-token',
     '/sign-in',
     '/sign-out',
-    'register',
+    '/register',
     '/generate-code',
     '/verify-code',
   ];
