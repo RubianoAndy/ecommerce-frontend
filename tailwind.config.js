319d55050a0d2e7f3175131d82effa355ab542ee
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-11-09 23:13:19 -0500

    feat: delete unnecesary fonts in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,ts,scss}"],
   theme: {
     extend: {
-      fontFamily: {
-        poppins: "'Poppins', sans-serif",
-        roboto: "'Roboto', sans-serif"
-      },
       colors: {
         'primary': '#2d5500',
         'secondary': '#fece3e',
