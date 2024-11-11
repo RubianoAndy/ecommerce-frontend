ae5cf9a9ccaa10e579683f52a0332578fa4df68e
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-11-10 22:05:57 -0500

    feat: adding table

--- src/app/account/components/users/users.component.ts
+++ src/app/account/components/users/users.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export default class UsersComponent {
-
+  items = new Array(10);
+  isChecked = true;
 }
